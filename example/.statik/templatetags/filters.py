061e3b2e8081793fdb5c76b783a2040c5b05ea02
Alej. N. Pérez <alxegod@gmail.com>
2016-12-12 12:39:11 +0100

    Updated Statik example project

--- example/.statik/templatetags/filters.py
+++ example/.statik/templatetags/filters.py
@@ -6,3 +6,10 @@ def urlify(path):
 		return None
 
 	return "<a href=\"{0!s}\">{0!s}</a>".format(path)
+
+@register.filter(name="mailify")
+def mailify(mail):
+	if mail is None:
+		return None
+
+	return "<a href=\"mailto:{0!s}\">{0!s}</a>".format(mail)
